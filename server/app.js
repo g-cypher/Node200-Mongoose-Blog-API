156770a6d9816113457ad1c2091cd52d5c81d092
Garrett <cyph17@gmail.com>
2019-07-14 14:20:27 -0700

    added new port and env file

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+
 mongoose.connect('mongodb://test:test12@ds117423.mlab.com:17423/heroku_r3fv70tz',{ useNewUrlParser: true });
 mongoose.Promise = Promise;
 
@@ -14,7 +15,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/blogs', require('./routes/blogs'));
 
 app.get('/', (req, res) => {
-    res.status(200).send();
+    res.status(200).send("Cheese");
 });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const server = require("./app");
+require('dotenv').config();
+const port = process.env.PORT;
 
 server.listen(8080, function() {
-    console.log('server is listening on http://localhost:8080')
+    console.log(`server is listening on http://localhost:${PORT}`)
 });

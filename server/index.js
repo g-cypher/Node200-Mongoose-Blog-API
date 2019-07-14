a9964210b507ea479b96e8a8b2ef0e2e02958bcd
Garrett <cyph17@gmail.com>
2019-07-14 14:31:39 -0700

    commit seppuku

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const server = require("./app");
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-server.listen(8080, function() {
+server.listen(PORT, function() {
     console.log(`server is listening on http://localhost:${PORT}`)
 });
